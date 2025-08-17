c7c24128bfbee8881e0a42086eb4160b53eba08b
kim24smel <kim24smel@gmail.com>
2025-08-17 11:47:00 +0800

    fix: 修复引反调试造成的偶尔无法正确加载配置问题

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
-﻿import disableDevtool from "disable-devtool";
+﻿import { addListener, launch } from 'devtools-detector';
 
 const isProd = process.env.NODE_ENV === "production";
 const enableConfigJS = process.env.VUE_APP_CONFIGJS == "true";
-const enableAntiDebugging = process.env.VUE_APP_DEBUGGING == "true";
+const enableAntiDebugging = process.env.VUE_APP_DEBUGGING == "true";;
 
 (async () => {
-  if (!isProd || !enableConfigJS) {
-    import('./config/index.js').then((res) => {
-      if (typeof window !== 'undefined') {
-        window.EZ_CONFIG = res.config || res;
-      }
-    });
-  }
-  
-  // 打包后开启反调试逻辑
-  if(isProd && enableAntiDebugging) {
-    disableDevtool()
+  try {
+    if (!isProd || !enableConfigJS) {
+      import('./config/index.js').then((res) => {
+        if (typeof window !== 'undefined') {
+          window.EZ_CONFIG = res.config || res;
+        }
+      });
+    }
+    
+    // 打包后开启反调试逻辑
+    if(isProd && enableAntiDebugging) {
+      addListener((isOpen) => {
+        if(isOpen) {
+          window.location.href = "https://google.com"
+        }
+      });
+
+      launch();
+    }
+  } catch (error) {
+    console.error(error);
   }
   
   await import('./appInit.js');
