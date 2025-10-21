18697d4c1c9745cfd3296ccc0aa2e08a5efd3d14
znt0948 <znt0948@users.noreply.github.com>
2025-10-21 12:28:07 +0800

    upgrade

--- src/i18n/locales/index.js
+++ src/i18n/locales/index.js
@@ -1,18 +1,30 @@
-﻿
-import zhCN from './zh-CN';
-import enUS from './en-US';
-import zhTW from './zh-TW';
-import jaJP from './ja-JP';
-import koKR from './ko-KR';
-import ruRU from './ru-RU';
-import faIR from './fa-IR';
-
-export default {
-  'zh-CN': zhCN,
-  'en-US': enUS,
-  'zh-TW': zhTW,
-  'ja-JP': jaJP,
-  'ko-KR': koKR,
-  'ru-RU': ruRU,
-  'fa-IR': faIR
-}; 
+﻿
+
+import zhCN from './zh-CN';
+
+import enUS from './en-US';
+
+import zhTW from './zh-TW';
+
+import jaJP from './ja-JP';
+
+import koKR from './ko-KR';
+
+import ruRU from './ru-RU';
+
+import faIR from './fa-IR';
+
+import viVN from './vi-VN';
+
+
+export default {
+  'zh-CN': zhCN,
+  'en-US': enUS,
+  'zh-TW': zhTW,
+  'ja-JP': jaJP,
+  'ko-KR': koKR,
+  'ru-RU': ruRU,
+  'fa-IR': faIR,
+  'vi-VN': viVN, 
+  'vi': viVN       
+};
